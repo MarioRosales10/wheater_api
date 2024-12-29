17db097be5ef74a86416adf2bd4f19bad00802e6
Omar del Rio Culebro <149621095+MostacillaDev@users.noreply.github.com>
2024-12-28 18:26:17 -0600

    ⚙️Inicializando proyecto Spring Boot

---
+++ src/main/java/com/example/wheater_api/WheaterApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wheater_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheaterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheaterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wheater_api/WheaterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wheater_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheaterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
